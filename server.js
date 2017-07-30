c657d48775ab23fcbe437800776635591e90d6a9
darthsator <darthsator@users.noreply.github.com>
2017-07-30 16:58:40 +0200

    path resolving

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ var express = require('express'),
     app     = express(),
     eps     = require('ejs'),
     morgan  = require('morgan'),
-    tsetup    = require('./tsetup.js');
+    tsetup  = require('./tsetup.js');
+
 
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(__dirname + '/views'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
