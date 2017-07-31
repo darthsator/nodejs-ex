38962688f9858f550dbec48e2ed31c990e811422
darthsator <darthsator@users.noreply.github.com>
2017-07-31 22:27:08 +0200

    without ips

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ app.get('/', function (req, res) {
     // Create a document with request IP and current time of request
 
     // col.insert({ip: req.ip, date: Date.now(), allips: req.ips, useragent: req.headers('User-Agent')});
-    col.insertOne({ip: req.ip, date: Date.now(), allips: req.ips});
+    // col.insertOne({ip: req.ip, date: Date.now(), allips: req.ips});
+    col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
       if (err) console.log(err);
       res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
