8e44954b4624e8c88f6d35bddc84fce3330eee99
darthsator <darthsator@users.noreply.github.com>
2017-08-08 20:42:58 +0200

    refactor

--- server.js
+++ server.js
@@ -207,11 +207,11 @@ app.get('/getAllEvents', function (req, res) {
 });
 
 app.post('/loadStats', function(req, res){
-  res.setHeader('Access-Control-Allow-Origin', '*');
+
   var postData = req.body;
   console.log(postData);
-  var evtMethod = postData.stats;
-  console.log(postData.stats);
+  var evtMethod = postData.cmd;
+  console.log(postData.cmd);
 
   if (!db) {
     initDb(function(err){});
