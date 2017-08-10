385e4a0d4e30b244af10e8971d680de30429d49c
darthsator <darthsator@users.noreply.github.com>
2017-08-10 23:17:00 +0200

    brace

--- server.js
+++ server.js
@@ -246,7 +246,8 @@ app.post('/loadStats', function(req, res){
           }
         },
         {$group : {
-          _id: "$session", count: { sum:"$tag"}
+            _id: "$session", count: { sum:"$tag"}
+          }
         },
         function(err, data) {
           if (err) console.log(err);
