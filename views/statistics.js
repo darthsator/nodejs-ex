3d8fc456ee718c2f40659e2e3e66b0ce0da25de1
darthsator <darthsator@users.noreply.github.com>
2017-08-12 22:10:37 +0200

    more charts

--- views/statistics.js
+++ views/statistics.js
@@ -1,5 +1,5 @@
 
-
+var lastCommand;
 $(document).ready(function() {
   $.ajax({
     dataType: "json",
@@ -25,8 +25,9 @@ function getMethodsFailed(data, textStatus, err) {
 
 function loadStats(command){
   command = $('#stats_method').val();
+  lastCommand = command;
   var com = {cmd:command};
-  if(command=='roomUtilisation') {
+  if(command == 'roomUtilisation') {
     com.rooms = dressrooms.map(function(r){return r.id});
   }
   $.ajax({
@@ -36,7 +37,9 @@ function loadStats(command){
     data: JSON.stringify(com),
     url: baseUrl+'loadStats'
   })
-  .done(createChart)
+  .done(function(data){
+    createChart(data, command)
+  })
   .fail(createChart);
   // https://stackoverflow.com/questions/40802071/pass-an-extra-argument-to-a-callback-function
   // https://stackoverflow.com/questions/21985201/pass-extra-parameters-to-jquery-ajax-promise-callback
@@ -44,42 +47,80 @@ function loadStats(command){
 
 function createChart(data) {
   if(!jQuery.isEmptyObject(data)) {
-     google.charts.load('current', {'packages':['corechart']});
-     // Set a callback to run when the Google Visualization API is loaded.
-     google.charts.setOnLoadCallback(drawChart);
-     var dataArray = [];
-     dataArray.push(['Session', 'Events', 'Products']);
-     data.sort(function(a,b) {
-       if(a.session) {
-         return parseInt(a.session - b.session);
-       } else {
-         return parseInt(a._id.session - b._id.session);
-       }
-     });
-     data.forEach(function(d){
-       var date = new Date(d._id.session);
-       dataArray.push([date.toISOString().substr(5,5)+" "+(date.toTimeString().substr(0, 8)), d.ne,d.ut]);
-     });
-     // Callback that creates and populates a data table,
-     // instantiates the pie chart, passes in the data and
-     // draws it.
-     function drawChart() {
-       // Create our data table.
-       var data = google.visualization.arrayToDataTable(dataArray);
+    google.charts.load('current', {'packages':['corechart']});
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+    var dataArray = [];
+    switch(lastCommand) {
+      case "sessionCount":
+        dataArray.push(['Session', 'Events', 'Products']);
+        data.sort(sortSessionCount);
+        data.forEach(function(d){
+          var date = new Date(d._id.session);
+          dataArray.push([date.toISOString().substr(5,5)+" "+(date.toTimeString().substr(0, 8)), d.ne,d.ut]);
+        });
+      break;
+      case "sessionsByHour":
+        dataArray.push(['Hour', 'Visits']);
+        data.sort(sortSessionHour);
+        data.forEach(function(d){
+          dataArray.push([d._id.hout, d.count]);
+        });
+      break;
+        case "roomUtilisation":
+        dataArray.push(['Room', 'Events', 'Products']);
+        data.sort(sortRoom);
+        data.forEach(function(d){
+          var date = new Date(d._id.session);
+          dataArray.push([date.toISOString().substr(5,5)+" "+(date.toTimeString().substr(0, 8)), d.ne,d.ut]);
+        });
+      default:
+      break;
+    }
 
-       // Set chart options
-       var options = {'title':$('#stats_method').val(),
-                    legend: { position: 'bottom' }
-                    };
+    // Callback that creates and populates a data table,
+    // instantiates the chart, passes in the data and
+    // draws it.
+    function drawChart() {
+      // Create our data table.
+      var data = google.visualization.arrayToDataTable(dataArray);
 
-     // Instantiate and draw our chart, passing in some options.
-       chart = new google.visualization.LineChart(document.getElementById('line_chart_div'));
-      //  google.visualization.events.addListener(chart, 'select', selectHandler);
-       chart.draw(data, options);
-     }
+      // Set chart options
+      var options = {'title':$('#stats_method').val().toUpperCase(),
+          legend: { position: 'bottom' }
+      };
+
+    // Instantiate and draw our chart, passing in some options.
+    switch(lastCommand) {
+      case "sessionCount":
+        chart = new google.visualization.LineChart(document.getElementById('line_chart_div'));
+      break;
+      case "sessionsByHour":
+        chart = new google.visualization.BarChart(document.getElementById('bar_chart_div'));
+      break;
+      case "roomUtilisation":
+        chart = new google.visualization.PieChart(document.getElementById('pie_chart_div'));
+      default:
+      break;
+    }
+    //  google.visualization.events.addListener(chart, 'select', selectHandler);
+    chart.draw(data, options);
+    }
   } else {
     appendToConsole('received empty object');
   }
+}
+
+function sortRoom(a,b) {
+  return parseInt(a._id.room - b._id.room);
+}
+
+function sortSessionHour(a,b) {
+  return parseInt(a._id.hour - b._id.hour);
+}
+
+function sortSessionCount(a,b) {
+  return parseInt(a._id.session - b._id.session);
 }
 
 // function selectHandler() {
